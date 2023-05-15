62df8ef7c31e805c12b6c2e322e4687ca137a893
epicmines33 <36407215+epicmines33@users.noreply.github.com>
2023-05-15 12:27:12 +0100

    Update modevotecore.js

--- modevotecore.js
+++ modevotecore.js
@@ -1,1 +1,15 @@
-console.log("test")
+var modes = ["goldquest", "cryptohack"]
+    var votes = [0, 0]
+    new Chart(document.getElementById('pie-chart'), {
+      type : 'pie',
+      data : {
+        labels : modes,
+        datasets : [{
+          backgroundColor : [ "#e63946", "#254BDD", "#ffbe0b", "#1d3557", "#326998" ],
+          data : votes
+        }]
+      },
+      options : {
+        responsive : true
+      }
+    });
